23269f15e575044c0b4799a528ff12ffc03d04ef
erns <erns@ernss-Mac.local>
2024-08-14 00:04:56 -0500

    Initial commit

---
+++ PathShape/PathShapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathShapeApp.swift
+//  PathShape
+//
+//  Created by erns on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct PathShapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
